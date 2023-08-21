733b7be850939cddf10dd2d17f493cd02ece6a4b
Resul Emre AYGAN <remreaygan@gmail.com>
2023-08-22 00:10:22 +0300

    added the ability to continue processing continuously added ability to reset added ability to keep results in memory

--- main.go
+++ main.go
@@ -4,74 +4,97 @@ Author: Resul Emre AYGAN
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
 type Calculator struct {
-	numberOne int
-	numberTwo int
+	result float64
 }
 
-func (c *Calculator) Add() {
-	fmt.Println("Addition of two numbers: ", c.numberOne+c.numberTwo)
+func (c *Calculator) Add(number float64) {
+	c.result += number
 }
 
-func (c *Calculator) Mul() {
-	fmt.Println("Multiplication of two numbers: ", c.numberOne*c.numberTwo)
+func (c *Calculator) Mul(number float64) {
+	c.result *= number
 }
 
-func (c *Calculator) Division() {
-	fmt.Println("Division of two numbers: ", c.numberOne/c.numberTwo)
+func (c *Calculator) Division(number float64) error {
+	if number != 0 {
+		c.result /= number
+		return nil
+	}
+	return fmt.Errorf("cannot divide by zero")
 }
 
-func (c *Calculator) Subtraction() {
-	fmt.Println("Subtraction of two numbers: ", c.numberOne-c.numberTwo)
+func (c *Calculator) Subtraction(number float64) {
+	c.result -= number
+}
+
+func getInput() string {
+	var input string
+	_, err := fmt.Scanln(&input)
+	if err != nil {
+		return ""
+	}
+	return input
 }
 
 func main() {
-	var numberOne, numberTwo int
+	fmt.Println("Welcome to Calculator!")
+	fmt.Println("Enter + for Addition: ")
+	fmt.Println("Enter * for Multiplication: ")
+	fmt.Println("Enter / for Division: ")
+	fmt.Println("Enter - for Subtraction: ")
+	fmt.Println("Enter reset for Reset: ")
+	fmt.Println("Enter exit for Exit.")
 
-	fmt.Print("Enter the first number: ")
-	_, err := fmt.Scanf("%d", &numberOne)
-	if err != nil {
-		return
-	}
+	var calc Calculator
 
-	fmt.Print("Enter the second number: ")
-	_, err = fmt.Scanf("%d", &numberTwo)
-	if err != nil {
-		return
-	}
+	for {
+		fmt.Println("Enter the operator (+, -, *, /) or 'reset' to reset or 'exit' to quit: ")
+		operator := getInput()
 
-	calc := Calculator{numberOne: numberOne, numberTwo: numberTwo}
-
-	value := 1
-
-	for value >= 1 {
-		fmt.Println("Enter 1 for Addition: ")
-		fmt.Println("Enter 2 for Multiplication: ")
-		fmt.Println("Enter 3 for Division: ")
-		fmt.Println("Enter 4 for Subtraction: ")
-		fmt.Println("Enter 5 for Exit.")
-
-		_, err2 := fmt.Scanf("%d", &value)
-		if err2 != nil {
-			return
-		}
-
-		switch value {
-		case 1:
-			calc.Add()
-		case 2:
-			calc.Mul()
-		case 3:
-			calc.Division()
-		case 4:
-			calc.Subtraction()
-		case 5:
-			value = 0
+		if operator == "exit" {
+			fmt.Println("Exiting calculator!")
 			break
-		default:
-			fmt.Println("Enter valid number!")
 		}
+
+		if operator == "reset" {
+			calc.result = 0
+			fmt.Println("Resetting calculator! Result:", calc.result)
+			continue
+		}
+
+		fmt.Print("Enter the number: ")
+		numberStr := getInput()
+
+		number, err := strconv.ParseFloat(numberStr, 64)
+
+		if err != nil {
+			fmt.Println("Invalid input for the second number!")
+			continue
+		}
+
+		switch operator {
+		case "+":
+			calc.Add(number)
+		case "*":
+			calc.Mul(number)
+		case "/":
+			err := calc.Division(number)
+			if err != nil {
+				return
+			}
+		case "-":
+			calc.Subtraction(number)
+		default:
+			fmt.Println("Invalid operator!")
+			continue
+		}
+
+		fmt.Printf("Result: %f\n", calc.result)
 	}
 }
